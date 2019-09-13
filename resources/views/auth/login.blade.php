d6736597a0843cd294c69e96cdaa530154e7e235
Paulo Mendes <paulomendesdigital@gmail.com>
2019-09-13 02:59:18 -0300

    Login translation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,14 +6,14 @@
     <div class="card ">
         <div class="card-header text-center">
             <img class="logo-img mb-3" src="{{url('assets/all/images/logo-mobile.jpg')}}" alt="logo">
-            <span class="splash-description">Por favor, insira suas informações</span>
+            <span class="splash-description">{{ __('Please, insert the informations:') }}</span>
         </div>
 
         <div class="card-body">
             <form method="POST" action="{{ route('login') }}">
                 @csrf
                 <div class="form-group">
-                    <input id="email" type="email" class="form-control form-control-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-mail">
+                    <input id="email" type="email" class="form-control form-control-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="{{ __('Email') }}">
 
                     @error('email')
                         <span class="invalid-feedback" role="alert">
@@ -23,7 +23,7 @@
                 </div>
 
                 <div class="form-group">
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror form-control-lg" name="password" required autocomplete="current-password" placeholder="Password">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror form-control-lg" name="password" required autocomplete="current-password" placeholder="{{ __('Password') }}">
 
                     @error('password')
                         <span class="invalid-feedback" role="alert">
@@ -34,7 +34,7 @@
 
                 <div class="form-group">
                     <label class="custom-control custom-checkbox">
-                        <input class="custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}><span class="custom-control-label">Lembrar-me</span>
+                        <input class="custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}><span class="custom-control-label">{{ __('Remember me') }}</span>
                     </label>
                 </div>
                 
@@ -44,7 +44,7 @@
         {{-- <div class="card-footer bg-white p-0  ">
             <div class="card-footer-item card-footer-item-bordered">
                 @if (Route::has('password.request'))
-                    <a href="{{ route('password.request') }}" class="footer-link">Esqueci a Sennha</a>
+                    <a href="{{ route('password.request') }}" class="footer-link">{{ __('Forgot the password?') }}</a>
                 @endif
             </div>
         </div> --}}
